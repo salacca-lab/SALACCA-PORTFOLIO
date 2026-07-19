151361b2b350f72722e3cd10c16a0a80508af12b
Salacca <salaccazlcc@gmail.com>
2026-07-19 17:14:50 +0700

    Update favicon

--- src/components/Contact.ts
+++ src/components/Contact.ts
@@ -14,16 +14,59 @@ export function Contact() {
 
         <div class="space-y-4">
 
-          <p>📧 Email : salaccazlcc@email.com</p>
+          <p>
+📧 Email :
+<a
+  href="mailto:salaccazlcc@email.com"
+  class="text-yellow-400 hover:underline"
+>
+  salaccazlcc@email.com
+</a>
+</p>
 
-          <p>📱 WhatsApp : +62 851-8777-0105</p>
+<p>
+📱 WhatsApp :
+<a
+  href="https://wa.me/6285187770105?text=Halo%20Salacca,%20saya%20tertarik%20dengan%20jasa%20Anda."
+  target="_blank"
+  class="text-yellow-400 hover:underline"
+>
+  +62 851-8777-0105
+</a>
+</p>
 
-          <p>📷 Instagram : @salacca</p>
+<p>
+📸 Instagram :
+<a
+  href="https://instagram.com/salacca_zlcc"
+  target="_blank"
+  class="text-yellow-400 hover:underline"
+>
+  @salacca
+</a>
+</p>
 
-          <p>🎵 TikTok : @salacca</p>
+<p>
+🎵 TikTok :
+<a
+  href="https://tiktok.com/@salaccaa1"
+  target="_blank"
+  class="text-yellow-400 hover:underline"
+>
+  @salaccaa1
+</a>
+</p>
 
-          <p>▶️ YouTube : Salacca</p>
-
+<p>
+▶️ YouTube :
+<a
+  href="https://youtube.com/@Salacca"
+  target="_blank"
+  class="text-yellow-400 hover:underline"
+>
+  Salacca
+</a>
+</p>
         </div>
 
       </div>
